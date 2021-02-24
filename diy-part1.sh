efd1f8e18f15948b7b5b2f6dac7218839220dfa7
changsoul <changsoul.wu@gmail.com>
2021-02-25 04:33:21 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-sed -i '$a src-git xucx https://github.com/kenzok8/small.git' feeds.conf.default
-sed -i '$a src-git fxxk https://github.com/kenzok8/openwrt-packages.git' feeds.conf.default
+#sed -i '$a src-git xucx https://github.com/kenzok8/small.git' feeds.conf.default
+#sed -i '$a src-git fxxk https://github.com/kenzok8/openwrt-packages.git' feeds.conf.default
