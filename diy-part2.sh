5680df38b8ca77e077185bf0d140a3fd3802103b
changsoul <changsoul.wu@gmail.com>
2021-03-09 03:24:40 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -14,8 +14,8 @@ sed -i 's/ssid=OpenWrt/ssid=TestWrt-${devidx}/' package/kernel/mac80211/files/li
 git clone https://github.com/openwrt/packages.git -b openwrt-21.02 $GITHUB_WORKSPACE/other_packages
 rm -rf feeds/packages/lang/golang/
 mv $GITHUB_WORKSPACE/other_packages/lang/golang/ feeds/packages/lang/
-mv -f $GITHUB_WORKSPACE/other_packages/net/xray-core/ feeds/packages/net/
-#sed -i 's/..\/..\/lang\/golang\/golang-package.mk/..\/..\/..\/feeds\/packages\/lang\/golang\/golang-package.mk/' package/network/xray-core/Makefile
+mv -f $GITHUB_WORKSPACE/other_packages/net/xray-core/ package/network/
+sed -i 's/..\/..\/lang\/golang\/golang-package.mk/..\/..\/..\/feeds\/packages\/lang\/golang\/golang-package.mk/' package/network/xray-core/Makefile
 rm -rf $GITHUB_WORKSPACE/other_packages
 
 # Fix some packages
