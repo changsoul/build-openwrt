7355a625c8c8a8e43e16f9349f8dde4f2245f94d
changsoul <changsoul.wu@gmail.com>
2021-03-09 00:56:31 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,9 +12,10 @@ sed -i 's/ssid=OpenWrt/ssid=TestWrt-${devidx}/' package/kernel/mac80211/files/li
 
 # Add other package
 git clone https://github.com/openwrt/packages.git -b openwrt-21.02 $GITHUB_WORKSPACE/other_packages
-#cp -rp $GITHUB_WORKSPACE/other_packages/net/xray-core/ packages/net/xray-core/
-#rm -rf $GITHUB_WORKSPACE/other_packages
+mv $GITHUB_WORKSPACE/other_packages/net/xray-core/ package/network/
+sed -i 's/..\/..\/lang\/golang\/golang-package.mk/..\/..\/..\/feeds\/packages\/lang\/golang\/golang-package.mk/' package/network/xray-core/Makefile
+rm -rf $GITHUB_WORKSPACE/other_packages
 
-
+# Fix some packages
 #find package/*/ feeds/*/ -maxdepth 2 -path "*luci-app-bypass/Makefile" | xargs -i sed -i 's/shadowsocksr-libev-ssr-redir/shadowsocksr-libev-alt/g' {}
 #find package/*/ feeds/*/ -maxdepth 2 -path "*luci-app-bypass/Makefile" | xargs -i sed -i 's/shadowsocksr-libev-ssr-server/shadowsocksr-libev-server/g' {}
